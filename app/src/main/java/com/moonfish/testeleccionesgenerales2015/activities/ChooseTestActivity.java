6525327db61312544584d5873b717c0b3c3ff6cd
JesusMSM <jesusmanuel93@hotmail.com>
2015-10-16 19:08:18 +0200

    Choose test hecho

--- app/src/main/java/com/moonfish/testeleccionesgenerales2015/activities/ChooseTestActivity.java
+++ app/src/main/java/com/moonfish/testeleccionesgenerales2015/activities/ChooseTestActivity.java
@@ -1,25 +1,67 @@
 package com.moonfish.testeleccionesgenerales2015.activities;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.moonfish.testeleccionesgenerales2015.R;
 
 /**
  * Created by Afll on 05/10/2015.
  */
-public class ChooseTestActivity extends ActionBarActivity {
+public class ChooseTestActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choosetest);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.actionBarColor)));
+        TextView estandar = (TextView) findViewById(R.id.modoEstandar);
+        TextView preguntas = (TextView) findViewById(R.id.preguntas);
+        TextView minutos = (TextView) findViewById(R.id.minutos);
+        TextView resultados = (TextView) findViewById(R.id.resultados);
+
+        //Modo detallado
+
+        TextView detallado = (TextView) findViewById(R.id.modoDetallado);
+        TextView preguntas2 = (TextView) findViewById(R.id.preguntas2);
+        TextView minutos2 = (TextView) findViewById(R.id.minutos2);
+        TextView resultados2 = (TextView) findViewById(R.id.resultados2);
+
+
+        estandar.setTypeface(Typeface.createFromAsset(getAssets(), "Titillium-Semibold.otf"));;
+        preguntas.setTypeface(Typeface.createFromAsset(getAssets(), "Titillium-Regular.otf"));;
+        minutos.setTypeface(Typeface.createFromAsset(getAssets(), "Titillium-Regular.otf"));;
+        resultados.setTypeface(Typeface.createFromAsset(getAssets(), "Titillium-Regular.otf"));;
+
+        detallado.setTypeface(Typeface.createFromAsset(getAssets(), "Titillium-Semibold.otf"));;
+        preguntas2.setTypeface(Typeface.createFromAsset(getAssets(), "Titillium-Regular.otf"));;
+        minutos2.setTypeface(Typeface.createFromAsset(getAssets(), "Titillium-Regular.otf"));;
+        resultados2.setTypeface(Typeface.createFromAsset(getAssets(), "Titillium-Regular.otf"));;
+
+    }
+
+    public void empezarTestEstandar(View v) {
+        Intent i = new Intent(getApplicationContext(),TestActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+        finish();
+    }
+
+    public void empezarTestDetallado(View v) {
+        Intent i = new Intent(getApplicationContext(),TestActivity.class);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
+        finish();
+
     }
 
     @Override
--- app/src/main/java/com/moonfish/testeleccionesgenerales2015/activities/TestActivity.java
+++ app/src/main/java/com/moonfish/testeleccionesgenerales2015/activities/TestActivity.java
@@ -1,7 +1,37 @@
 package com.moonfish.testeleccionesgenerales2015.activities;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.moonfish.testeleccionesgenerales2015.R;
+
 /**
  * Created by Afll on 05/10/2015.
  */
-public class TestActivity {
+public class TestActivity extends ActionBarActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.actionBarColor)));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
